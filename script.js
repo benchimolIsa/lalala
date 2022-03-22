2ee2dc0f2ef1934505d030233833e509042cee39
benchimolIsa <102107984+benchimolIsa@users.noreply.github.com>
2022-03-21 23:46:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function mudarTema() {
+  document.body.classList.toggle("dark");
+}
+
+// desafios
+//Terminar todo o design que está no figma (disponível abaixo)
+// Adicionar suas redes sociais de forma que quem clique consiga abrí-las
+// <a href="https://codepen.io/benchimolisa/pen/XWVdMNB">
+//<a href="https://codepen.io/benchimolisa/pen/mdpPYVW">
+// <a href="https://codepen.io/benchimolisa/pen/MWrjaQa">
